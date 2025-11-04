7583d9e8363da311effad815039a9b9c94807041
Benosaure <benosaure.krit@gmail.com>
2025-11-04 11:43:34 +0100

    <create class> : creation of the Team class and its data

---
+++ core/src/main/java/com/T_jav_502/Theotterspace/teams/Team.java
@@ -0,0 +1,37 @@
+package com.T_jav_502.Theotterspace.teams;
+import  com.T_jav_502.Theotterspace.units.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.T_jav_502.Theotterspace.teams.Team.Species.OTTER;
+
+public class Team {
+    public enum Species {
+        OTTER,
+        WOLF
+    }
+    private final Species curentSpecies;
+
+    private List<aUnit> unitList = new ArrayList<aUnit>() ;
+
+    public Team(Species species ) {
+        switch(species) {
+            case WOLF :
+                curentSpecies = Species.WOLF ;
+                break;
+            case OTTER :
+                curentSpecies = OTTER ;
+                break;
+        }
+
+    }
+
+    public Species getCurentSpecies() {
+        return curentSpecies;
+    }
+
+    public List<aUnit> getUnits() {
+        return unitList;
+    }
+}
