d54e637f98d2fa82ad79652b83d0e30e1a9e1c72
Rayan Poussuet <rayan.poussuet@epitech.eu>
2025-11-14 14:51:29 +0100

    fix(display): added display images for units

---
+++ assets/Maps/units.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="units" tilewidth="32" tileheight="48" tilecount="2" columns="2">
+ <image source="../units.png" width="64" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="team" value="WOLF"/>
+   <property name="type" value="Infantry"/>
+  </properties>
+ </tile>
+</tileset>
