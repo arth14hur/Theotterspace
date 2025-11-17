0bc0389f67ab9720abb0942957df8ee0cdef9254
Rayan Poussuet <rayan.poussuet@epitech.eu>
2025-11-17 11:27:35 +0100

    style(units): remodeled the unit tileset

--- assets/Maps/units.tsx
+++ assets/Maps/units.tsx
@@ -1,10 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="units" tilewidth="32" tileheight="48" tilecount="2" columns="2">
- <image source="../units.png" width="64" height="48"/>
+<tileset version="1.10" tiledversion="1.11.2" name="units" tilewidth="32" tileheight="48" tilecount="6" columns="3">
+ <image source="../units.png" width="96" height="96"/>
  <tile id="0">
   <properties>
    <property name="team" value="WOLF"/>
    <property name="type" value="Infantry"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="team" value="WOLF"/>
+   <property name="type" value="Blaster"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="team" value="WOLF"/>
+   <property name="type" value="Heavy"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="team" value="OTTER"/>
+   <property name="type" value="Infantry"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="team" value="OTTER"/>
+   <property name="type" value="Blaster"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="team" value="OTTER"/>
+   <property name="type" value="Heavy"/>
   </properties>
  </tile>
 </tileset>
